559405d8fd4a052480d8b9213012215291a2325d
SalvadorLekan <lekansalvador@gmail.com>
2021-05-06 19:16:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Rock, Paper, Scissors solution
+
+This is a solution to the [Rock, Paper, Scissors challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Play Rock, Paper, Scissors against the computer
+- Maintain the state of the score after refreshing the browser _(optional)_
+- **Bonus**: Play Rock, Paper, Scissors, Lizard, Spock against the computer _(optional)_
+
+### Screenshot
+
+![](./screenshot.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/SalvadorLekan/rock-paper-scissors)
+- Live Site URL: [Add live site URL here](https://SalvadorLekan.github.io/rock-paper-scissors)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Website - [Salvador lekan](https://www.salvadorlekan.com.ng)
+- Frontend Mentor - [@salvadorlekan](https://www.frontendmentor.io/profile/salvadorlekan)
+- Twitter - [@salvadorlekan](https://www.twitter.com/salvadorlekan)
+
+
---
+++ index.js
@@ -0,0 +1,51 @@
+const cover = document.getElementById("cover")
+const restart = document.getElementById("restart")
+const yourMove = document.getElementById("your-move")
+const game = document.getElementById("game")
+const moveButtons = document.querySelectorAll("[data-move]")
+const announcement = document.querySelector("#result p")
+const compImage = document.getElementById("comp-move")
+const score = document.getElementById("score")
+
+const rpsls = ["rock", "paper", "scissors", "lizard", "spock"]
+
+const winningMoves = {
+	scissors: new Set(["paper", "lizard"]),
+	spock: new Set(["rock", "scissors"]),
+	paper: new Set(["rock", "spock"]),
+	lizard: new Set(["spock", "paper"]),
+	rock: new Set(["lizard", "scissors"])
+}
+
+moveButtons.forEach(
+	element =>
+		(element.onclick = () => {
+			const move = element.dataset.move
+			const cmove = computerMove()
+			yourMove.src = `/images/icon-${move}.svg`
+			yourMove.alt = move
+			yourMove.parentElement.setAttribute("data-result", move)
+			compImage.parentElement.setAttribute("data-result", cmove)
+			compImage.src = `/images/icon-${cmove}.svg`
+			compImage.alt = cmove
+			const result = play(move, cmove)
+			announcement.innerHTML = `YOU ${result}`
+			cover.classList.add("hide")
+			game.classList.remove("hide")
+			if (result === "WIN") score.innerText++
+		})
+)
+restart.onclick = () => {
+	cover.classList.remove("hide")
+	game.classList.add("hide")
+}
+
+function computerMove() {
+	return rpsls[Math.floor(Math.random() * rpsls.length)]
+}
+
+function play(you, computer) {
+	if (you === computer) return "TIE"
+	if (winningMoves[you].has(computer)) return "WIN"
+	return "LOSE"
+}
